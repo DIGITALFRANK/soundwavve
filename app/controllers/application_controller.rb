ea646b242f74fb8db23939318be5df2320464137
FRANCIS AGBODJI <frank@digitalfrank.com>
2018-06-01 19:52:07 -0400

    configured registration & login for users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user!
+    before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+
+    def after_sign_in_path_for(resource)
+        feed_path
+    end
+
+
+
+
+
+    protected
+
+    def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:img_url, :first_name, :last_name, :email, :password])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:img_url, :first_name, :last_name, :email, :password])
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -30,8 +30,9 @@ ActiveRecord::Schema.define(version: 2018_06_01_004651) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
+    t.string "img_url"
+    t.string "first_name", null: false
+    t.string "last_name", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
